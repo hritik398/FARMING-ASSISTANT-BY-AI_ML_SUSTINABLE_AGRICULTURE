f053f54b89ff8d82c099affc57a4fe22ac059cb7
Hritik Prajapati <hritikprajapati398@gmail.com>
2025-08-31 11:07:04 +0530

    Create capture_screen.dart

---
+++ lib/🧭 lib/🧑‍💻 /screen/capture_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class CaptureScreen extends StatelessWidget {
+const CaptureScreen({super.key});
+@override
+Widget build(BuildContext context) {
+return const Scaffold(
+body: Center(child: Text('Optional extra screen for batch capture /
+history.')),
+);
+}
+}
